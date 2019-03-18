2c0420352857f097a2e3ed4c5b6ff22549b247fe
akintoluvic@gmail.com <akintoluvic@gmail.com>
2019-03-19 00:14:46 +0100

    created aiPlayer

--- script.js
+++ script.js
@@ -27,7 +27,10 @@ function startGame() {
 }
 
 function turnClick(square) {
-    turn(square.target.id, huPlayer)
+    if (typeof origBoard[square.target.id] == 'number') {
+      turn(square.target.id, huPlayer);
+      if(!checkTie()) turn(bestSpot(), aiPlayer);  
+    }
 }
 
 function turn(squareId, player) {
@@ -56,6 +59,32 @@ function gameOver(gameWon) {
             = gameWon.player == huPlayer ? "blue" : "red";
     }
     for (var i = 0; i < cells.length; i++) {
-        cells[i].removeEventListener('click', turnClick, false)
+        cells[i].removeEventListener('click', turnClick, false);
     }
+    declareWinner(gameWon.player == huPlayer ? "You won!" : "You lost!")
+}
+
+function declareWinner(who) {
+    document.querySelector(".endgame").style.display = "block";
+    document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+    return origBoard.filter(s => typeof s== 'number');
+}
+
+function bestSpot() {
+    return emptySquares()[0];
+}
+
+function checkTie() {
+    if (emptySquares().length === 0) {
+       for (var i = 0; i < cells.length; i++) {
+           cells[i].style.backgroundColor = "green";
+           cells[i].removeEventListener('click', turnClick, false);
+       } 
+       declareWinner("Tie Game!")
+       return true;
+    }
+    return false;
 }
