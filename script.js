e63ca2ee48476d466744f08481e648d7a18446ab
akintoluvic@gmail.com <akintoluvic@gmail.com>
2019-03-18 23:58:54 +0100

    created gameOver function

--- script.js
+++ script.js
@@ -42,9 +42,20 @@ function checkWin(board, player) {
     (e === player) ? a.concat(i) : a, []);
     let gameWon = null;
     for (let [index, win] of winCombos.entries()) {
-        if (win.every(elem => plays.indexOf(elem > -1)) {
+        if (win.every(elem => plays.indexOf(elem) > -1)) {
             gameWon = {index: index, player: player};
+            break;
         }
     }
     return gameWon;
+}
+
+function gameOver(gameWon) {
+    for (let index of winCombos[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor
+            = gameWon.player == huPlayer ? "blue" : "red";
+    }
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].removeEventListener('click', turnClick, false)
+    }
 }
